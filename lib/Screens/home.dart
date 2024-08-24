bdf9c38e3c477306ab448186ed73524fbd00f609
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 <abhinavlmna>
2024-08-24 14:55:06 +0530

    bug

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -11,6 +11,7 @@ import 'package:lyrica/Screens/search.dart';
 import 'package:lyrica/database/db_functions.dart';
 // import 'package:on_audio_query/on_audio_query.dart';
 import 'package:lyrica/database/db_model.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class Homepage extends StatefulWidget {
@@ -249,7 +250,7 @@ class _HomeTabState extends State<HomeTab> {
   //   return getAllSongs();
   // }
 
-  // final _audioQuery = OnAudioQuery();
+  final _audioQuery = OnAudioQuery();
   @override
   void initState() {
     // TODO: implement initState
@@ -265,149 +266,147 @@ class _HomeTabState extends State<HomeTab> {
   Widget build(BuildContext context) {
     MediaQueryData mediaQuery = MediaQuery.of(context);
 
-    // return FutureBuilder<List<SongModel>>(
-    //     future: _audioQuery.querySongs(
-    //         sortType: null,
-    //         orderType: OrderType.ASC_OR_SMALLER,
-    //         uriType: UriType.EXTERNAL,
-    //         ignoreCase: true),
-    //     builder: (context, item) {
-    //       if (item.data == null) {
-    //         return Center(
-    //           child: CircularProgressIndicator(),
-    //         );
-    //       }
-    //       if (item.data!.isEmpty) {
-    //         return Center(
-    //           child: Column(
-    //             children: [
-    //               Lottie.asset('assets/images/nodata.json'),
-    //               Text(
-    //                 'No Songs found',
-    //                 style: TextStyle(
-    //                     fontSize: 20,
-    //                     color: Colors.white,
-    //                     fontWeight: FontWeight.bold),
-    //               )
-    //             ],
-    //           ),
-    //         );
-    //       }
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8),
-          child: Container(
-            height: mediaQuery.size.height * 0.4,
-            width: mediaQuery.size.width * 0.97,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20)),
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 60,
-                  left: 30,
-                  child: Lottie.asset(
-                    'assets/images/music1.json',
-                    height: mediaQuery.size.height * 0.3,
-                    width: mediaQuery.size.width * 0.8,
+    return FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+            sortType: null,
+            orderType: OrderType.ASC_OR_SMALLER,
+            uriType: UriType.EXTERNAL,
+            ignoreCase: true),
+        builder: (context, item) {
+          if (item.data == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (item.data!.isEmpty) {
+            return Center(
+              child: Column(
+                children: [
+                  Lottie.asset('assets/images/nodata.json'),
+                  Text(
+                    'No Songs found',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            );
+          }
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  height: mediaQuery.size.height * 0.4,
+                  width: mediaQuery.size.width * 0.97,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 60,
+                        left: 30,
+                        child: Lottie.asset(
+                          'assets/images/music1.json',
+                          height: mediaQuery.size.height * 0.3,
+                          width: mediaQuery.size.width * 0.8,
+                        ),
+                      ),
+                      Positioned(
+                        top: 40,
+                        child: Builder(
+                          builder: (context) {
+                            return IconButton(
+                              onPressed: () {
+                                Homepage.scaffoldKey.currentState?.openDrawer();
+                              },
+                              icon: Icon(
+                                Icons.menu,
+                                size: 40,
+                                color: Colors.white,
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Positioned(
-                  top: 40,
-                  child: Builder(
-                    builder: (context) {
-                      return IconButton(
-                        onPressed: () {
-                          Homepage.scaffoldKey.currentState?.openDrawer();
-                        },
-                        icon: Icon(
-                          Icons.menu,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                      );
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 15, top: 8),
-            child: Text(
-              'All Songs',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
               ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text('music'),
-              );
-              // return ListTile(
-              //   leading: QueryArtworkWidget(
-              //     nullArtworkWidget: Icon(Icons.headphones),
-              //     id: item.data![index].id,
-              //     type: ArtworkType.AUDIO,
-              //   ),
-              //   title: Text(
-              //     item.data![index].displayNameWOExt,
-              //     maxLines: 1,
-              //     overflow: TextOverflow.ellipsis,
-              //     style: TextStyle(
-              //         fontSize: 20,
-              //         fontWeight: FontWeight.bold,
-              //         color: Colors.black),
-              //   ),
-              //   subtitle: Text(
-              //     item.data![index].title,
-              //     maxLines: 1,
-              //     overflow: TextOverflow.ellipsis,
-              //     style: TextStyle(fontSize: 17, color: Colors.black),
-              //   ),
-              //   trailing: IconButton(
-              //     onPressed: () {
-              //       int playsongid = item.data![index].id;
-              //       showModalBottomSheet(
-              //           shape: RoundedRectangleBorder(
-              //               borderRadius: BorderRadius.only(
-              //                   bottomLeft: Radius.circular(40),
-              //                   bottomRight: Radius.circular(40))),
-              //           context: context,
-              //           builder: (context) {
-              //             return Container(
-              //               height: mediaQuery.size.height * 0.30,
-              //               decoration: BoxDecoration(
-              //                   borderRadius: BorderRadius.only(
-              //                       topLeft: Radius.circular(40),
-              //                       topRight: Radius.circular(40)),
-              //                   color: Colors.black),
-              //             );
-              //           });
-              //     },
-              //     icon: Icon(Icons.more_vert),
-              //   ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 8),
+                  child: Text(
+                    'All Songs',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: QueryArtworkWidget(
+                        nullArtworkWidget: Icon(Icons.headphones),
+                        id: item.data![index].id,
+                        type: ArtworkType.AUDIO,
+                      ),
+                      title: Text(
+                        item.data![index].displayNameWOExt,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      subtitle: Text(
+                        item.data![index].title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 17, color: Colors.black),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          int playsongid = item.data![index].id;
+                          showModalBottomSheet(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(40),
+                                      bottomRight: Radius.circular(40))),
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  height: mediaQuery.size.height * 0.30,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(40),
+                                          topRight: Radius.circular(40)),
+                                      color: Colors.black),
+                                );
+                              });
+                        },
+                        icon: Icon(Icons.more_vert),
+                      ),
 
-              //   // need to complete bottomsheet
-              // );
-            },
-          ),
-        ),
-      ],
-    );
+                      // need to complete bottomsheet
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
